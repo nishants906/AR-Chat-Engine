4c8e4439f4a96af989f8c41fc1e44f53395d34a2
Nishant <sardana.nishant@gmail.com>
2019-03-11 13:57:46 +0530

    AR chat completed

--- app/src/main/java/com/example/nishant/ar_chat_engine/MainActivity.java
+++ app/src/main/java/com/example/nishant/ar_chat_engine/MainActivity.java
@@ -33,6 +33,8 @@ import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.BaseArFragment;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -41,49 +43,170 @@ public class MainActivity extends AppCompatActivity {
     ArFragment arFragment;
     private ArSceneView arSceneView;
     private boolean installRequested;
-    ViewRenderable vi;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (!DemoUtils.checkIsSupportedDeviceOrFinish(this)) {
+            // Not a supported device.
+            return;
+        }
+
         setContentView(R.layout.activity_main);
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ar_scene_view);
         arSceneView = arFragment.getArSceneView();
 
- d
-        arFragment.setOnTapArPlaneListener(new BaseArFragment.OnTapArPlaneListener() {
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ar_scene_view);
+        arSceneView = arFragment.getArSceneView();
+
+        arFragment.getPlaneDiscoveryController().hide();
+        arFragment.getPlaneDiscoveryController().setInstructionView(null);
+
+
+        try {
+            Session session = DemoUtils.createArSession(this, installRequested);
+            Config config = new Config(session);
+            config.setUpdateMode(Config.UpdateMode.LATEST_CAMERA_IMAGE);
+            config.setPlaneFindingMode(Config.PlaneFindingMode.HORIZONTAL);
+            session.configure(config);
+            arSceneView.setupSession(session);
+
+        } catch (UnavailableException e) {
+            Log.e("erroroccur",e.toString());
+        }
+
+
+
+
+//                DemoUtils.displayError(this, "Unable to load renderable", ex);
+//            }
+//
+//            return null;
+//        });
+
+
+            arFragment.setOnTapArPlaneListener(new BaseArFragment.OnTapArPlaneListener() {
+                @Override
+                public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
+                    if (plane.getType()!=Plane.Type.HORIZONTAL_UPWARD_FACING){
+                        return;
+                    }
+                    Anchor anchor = hitResult.createAnchor();
+                    placeObject(arFragment,anchor);
+
+                }
+            });
+
+        }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void placeObject(ArFragment fragment, Anchor anchor){
+
+        ViewRenderable.builder()
+                .setView(fragment.getContext(), R.layout.planet_card_view)
+                .build()
+                .thenAccept(renderable -> addnodetoscene(fragment, anchor, renderable))
+                .exceptionally(throwable -> {
+                    Log.d("enter1",throwable.getMessage());
+
+                    return null;
+                });
+
+
+
+    }
+
+    public void addnodetoscene(ArFragment fragment, Anchor anchor,ViewRenderable renderable){
+
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        anchorNode.setParent(fragment.getArSceneView().getScene());
+
+        Node node = new Node();
+        node.setLocalPosition(new Vector3(0f,-0.121f,0));
+        node.setParent(anchorNode);
+        node.setRenderable(renderable);
+        Log.d("node-created","created");
+
+        View v = renderable.getView();
+
+        RecyclerView rv = v.findViewById(R.id.rv_chat);
+        ChatAdapter chatAdapter = new ChatAdapter(getApplicationContext());
+        LinearLayoutManager lm = new LinearLayoutManager(getApplicationContext());
+        rv.setLayoutManager(lm);
+        rv.setAdapter(chatAdapter);
+
+
+
+        Call<List<ChatUser>> chatUserCall = client.getchatuser(getSharedPreferences("Tokenkey", MODE_PRIVATE).getString("token", "token1"), String.valueOf(pageposition));
+        chatUserCall.enqueue(new Callback<List<ChatUser>>() {
             @Override
-            public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
+            public void onResponse(Call<List<ChatUser>> call, Response<List<ChatUser>> response) {
+                list = new ArrayList<>();
 
-                Log.d("hitresult","hitresult");
-                AnchorNode anchorNode = new AnchorNode(hitResult.createAnchor());
-                anchorNode.setParent(arFragment.getArSceneView().getScene());
+                if (response.body()!=null) {
+                    if (response.body().size() > 0) {
 
-                Node infoCard = new Node();
-                infoCard.setParent(anchorNode);
-                infoCard.setRenderable(vi);
+                        for (int i = 0; i < response.body().size(); i++) {
 
-                RecyclerView rv = vi.getView().findViewById(R.id.rv_chat);
-                ChatAdapter chatAdapter = new ChatAdapter(getApplicationContext());
-                LinearLayoutManager lm = new LinearLayoutManager(getApplicationContext());
-                rv.setLayoutManager(lm);
-                rv.setAdapter(chatAdapter);
+                            list.add(response.body().get(i));
+                        }
+
+                        progressBar.setVisibility(View.GONE);
+                        lm = new LinearLayoutManager(getApplicationContext());
+                        chatlistAdapter = new ChatHistoryAdapter(getApplicationContext(), list,HomeActivity.userid);
+                        rv_chat.setAdapter(chatlistAdapter);
+                        rv_chat.setLayoutManager(lm);
+                        rv_chat.setHasFixedSize(true);
+                        rv_chat.addOnScrollListener(new EndlessRecyclerOnScrollListener(lm) {
+                            @Override
+                            public void onLoadMore(int current_page)
+                            {
+                                ploader.setVisibility(View.VISIBLE);
+                                Call<List<ChatUser>> chatUserCall = client.getchatuser(getSharedPreferences("Tokenkey", MODE_PRIVATE).getString("token", "token1"), String.valueOf(current_page));
+                                chatUserCall.enqueue(new Callback<List<ChatUser>>() {
+                                    @Override
+                                    public void onResponse(Call<List<ChatUser>> call, Response<List<ChatUser>> response) {
+                                        ploader.setVisibility(View.GONE);
+                                        if (response.body()!=null) {
+
+                                            for(int i =0;i<response.body().size();i++){
+
+                                                list.add(response.body().get(i));
+
+                                                chatlistAdapter.notifyItemRangeInserted(chatlistAdapter.getItemCount(),list.size()-1);
+                                            }
+
+
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onFailure(Call<List<ChatUser>> call, Throwable t) {
+//                        Toast.makeText(ChatHistory.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+
+                                    }
+                                });
+
+                            }
+                        });
+                    } else {
+                        rl_nochat.setVisibility(View.VISIBLE);
+                        progressBar.setVisibility(View.GONE);
+                    }
+                }
+            }
+            @Override
+            public void onFailure(Call<List<ChatUser>> call, Throwable t) {
+//                Toast.makeText(ChatHistory.this, t.getMessage(), Toast.LENGTH_SHORT).show();
 
             }
         });
-//
-//        CompletableFuture<ViewRenderable> recyc = ViewRenderable.builder().setView(this, R.layout.planet_card_view).build();
-//
-//        try {
-//            vi = recyc.get();
-//        } catch (ExecutionException e) {
-//            e.printStackTrace();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+
+
+
     }
-
-
-
 }
